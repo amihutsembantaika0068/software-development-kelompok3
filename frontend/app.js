161ff9e34054063a6db9c6baa5e273f05e24abf6
amihutsembantaika0068 <amibantaika@gmail.com>
2026-04-20 07:04:52 +0700

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,26 @@
-// script frontend akan ditambahkan
+function showSection(id) {
+  // sembunyikan semua section
+  document.querySelectorAll(".card").forEach(section => {
+    section.classList.remove("active");
+  });
+
+  // tampilkan section yang dipilih
+  document.getElementById(id).classList.add("active");
+}
+
+function identifikasiDNA() {
+  // ambil input DNA
+  let dna = document.getElementById("dnaInput").value.trim();
+
+  // tampilkan DNA yang diinput
+  document.getElementById("outDNA").innerText = dna || "-";
+
+  // hasil dummy (sementara)
+  document.getElementById("outSpecies").innerText = "Oncorhynchus nerka";
+  document.getElementById("outGenus").innerText = "Oncorhynchus";
+  document.getElementById("outFamily").innerText = "Salmonidae";
+  document.getElementById("outConfidence").innerText = "92%";
+
+  // pindah ke halaman hasil
+  showSection("hasil");
+}
